36f6bfcb58e41299d89c102e38cdd85a6d8e5623
Lucas Damazio da Costa <lucas.damaziocosta@outlook.com>
2019-03-30 15:49:15 -0300

    BATMAN

---
+++ lib/ultrafastsum.rb
@@ -0,0 +1,6 @@
+require "ultrafastsum/version"
+
+module Ultrafastsum
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ultrafastsum/version.rb
@@ -0,0 +1,3 @@
+module Ultrafastsum
+  VERSION = "0.1.0"
+end
